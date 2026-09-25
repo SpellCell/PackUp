e2820b7894c68e7c798f7792e9ada01e02096a63
SpellCell <vipul.raj_cs23@gla.ac.in>
2026-09-25 10:09:12 +0530

    Add authenticated password change endpoint

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import bcrypt from "bcrypt";
 
 // ===========================================
 // Get Logged In User Profile
@@ -98,6 +99,117 @@ export const updateProfile = asyncHandler(async (req, res) => {
         message: "Profile Updated Successfully",
 
         user
+
+    });
+
+});
+
+// ===========================================
+// Change Password
+// ===========================================
+
+export const changePassword = asyncHandler(async (req, res) => {
+
+    const {
+        currentPassword,
+        newPassword
+    } = req.body;
+
+    if (!currentPassword || !newPassword) {
+
+        const error = new Error(
+            "Current password and new password are required"
+        );
+        error.statusCode = 400;
+        throw error;
+
+    }
+
+    if (newPassword.length < 8) {
+
+        const error = new Error(
+            "New password must be at least 8 characters"
+        );
+        error.statusCode = 400;
+        throw error;
+
+    }
+
+    if (!/[A-Z]/.test(newPassword)) {
+
+        const error = new Error(
+            "New password must contain at least one uppercase letter"
+        );
+        error.statusCode = 400;
+        throw error;
+
+    }
+
+    if (!/[a-z]/.test(newPassword)) {
+
+        const error = new Error(
+            "New password must contain at least one lowercase letter"
+        );
+        error.statusCode = 400;
+        throw error;
+
+    }
+
+    if (!/[0-9]/.test(newPassword)) {
+
+        const error = new Error(
+            "New password must contain at least one number"
+        );
+        error.statusCode = 400;
+        throw error;
+
+    }
+
+    if (currentPassword === newPassword) {
+
+        const error = new Error(
+            "New password must be different from your current password"
+        );
+        error.statusCode = 400;
+        throw error;
+
+    }
+
+    const user = await User.findById(req.user._id).select("+password");
+
+    if (!user) {
+
+        const error = new Error("User Not Found");
+        error.statusCode = 404;
+        throw error;
+
+    }
+
+    const passwordMatches =
+        await bcrypt.compare(
+            currentPassword,
+            user.password
+        );
+
+    if (!passwordMatches) {
+
+        const error = new Error(
+            "Current password is incorrect"
+        );
+        error.statusCode = 400;
+        throw error;
+
+    }
+
+    user.password = newPassword;
+
+    await user.save();
+
+    res.status(200).json({
+
+        success: true,
+
+        message: "Password changed successfully"
 
     });
 
