06c314c32bf2a2cf202f049e82cb220807ac4da0
SpellCell <vipul.raj_cs23@gla.ac.in>
2026-09-25 07:19:49 +0530

    Add mark notification as read endpoint

--- server/src/controllers/notificationController.js
+++ server/src/controllers/notificationController.js
@@ -38,3 +38,61 @@ export const getNotifications = async (req, res) => {
     }
 
 };
+
+export const markNotificationAsRead = async (req, res) => {
+
+    try {
+
+        const notification =
+            await Notification.findOneAndUpdate(
+
+                {
+                    _id: req.params.id,
+                    recipient: req.user._id
+                },
+
+                {
+                    isRead: true
+                },
+
+                {
+                    new: true
+                }
+
+            );
+
+        if (!notification) {
+
+            return res.status(404).json({
+
+                success: false,
+
+                message: "Notification not found"
+
+            });
+
+        }
+
+        res.status(200).json({
+
+            success: true,
+
+            notification
+
+        });
+
+    } catch (error) {
+
+        console.log(error);
+
+        res.status(500).json({
+
+            success: false,
+
+            message: "Internal Server Error"
+
+        });
+
+    }
+
+};
