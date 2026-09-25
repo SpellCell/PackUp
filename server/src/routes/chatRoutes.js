b983c9b52242f349c79a8cadd6c25eb7b7ce33ff
SpellCell <vipul.raj_cs23@gla.ac.in>
2026-09-25 07:48:20 +0530

    Add chat summary and read routes

--- server/src/routes/chatRoutes.js
+++ server/src/routes/chatRoutes.js
@@ -3,10 +3,24 @@ import express from "express";
 import protect from "../middleware/authMiddleware.js";
 
 import {
-    getChatHistory
+    getChatHistory,
+    getChatSummaries,
+    markChatAsRead
 } from "../controllers/chatController.js";
 
 const router = express.Router();
+
+router.get(
+    "/summaries",
+    protect,
+    getChatSummaries
+);
+
+router.patch(
+    "/:tripId/read",
+    protect,
+    markChatAsRead
+);
 
 router.get(
     "/:tripId/history",
